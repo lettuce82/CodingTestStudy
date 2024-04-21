1763635cf5b2af5a97c1cf2aa0514add863245ca
lettuce82 <152861713+lettuce82@users.noreply.github.com>
2024-04-22 07:46:36 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 328 ms, Memory: 18044 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.nextLine(); // 개행 문자 처리
+
+		for (int i = 0; i < num; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+    }
+}
