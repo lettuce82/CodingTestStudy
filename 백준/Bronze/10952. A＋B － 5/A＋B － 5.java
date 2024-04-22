3b0457a6e553e0f242a691470a2109042bde5a64
lettuce82 <152861713+lettuce82@users.noreply.github.com>
2024-04-22 12:37:14 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,26 +1,25 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-		List<Integer> sum = new ArrayList<>();
-		int num1;
-		int num2;
-
-		while (true) {
-			num1 = sc.nextInt();
-			num2 = sc.nextInt();
-			if (num1 == 0 && num2 == 0) {
-				break;
-			} else {
-				sum.add(num1 + num2);
-			}
-		}
-
-		for (Integer integer : sum) {
-			System.out.println(integer);	
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        while (true) {
+            String line = reader.readLine();
+            if (line == null || line.equals("0 0")) {
+                break;
+            }
+            
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            int num1 = Integer.parseInt(tokenizer.nextToken());
+            int num2 = Integer.parseInt(tokenizer.nextToken());
+            
+            System.out.println(num1 + num2);
+        }
+        
+        reader.close(); // 스트림을 사용한 후에는 반드시 닫아주어야 합니다.
     }
 }
