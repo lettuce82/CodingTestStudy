20b5c4d607203af04cc5052b74e5ad6c7287715b
lettuce82 <152861713+lettuce82@users.noreply.github.com>
2024-04-22 12:38:26 +0900

    [Bronze V] Title: A+B - 4, Time: 128 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        while (true) {
+            String line = reader.readLine();
+            if (line == null) {
+                break;
+            }
+            
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            int num1 = Integer.parseInt(tokenizer.nextToken());
+            int num2 = Integer.parseInt(tokenizer.nextToken());
+            
+            System.out.println(num1 + num2);
+        }
+        
+        reader.close(); // 스트림을 사용한 후에는 반드시 닫아주어야 합니다.
+    }
+}
