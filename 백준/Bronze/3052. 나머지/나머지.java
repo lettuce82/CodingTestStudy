40789aba8daebbc524224bf77695422c56cbc12a
lettuce82 <152861713+lettuce82@users.noreply.github.com>
2024-08-15 20:01:18 +0900

    [Bronze II] Title: 나머지, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+/*
+	문제
+	두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는 1, 2, 0, 2이다.
+	수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
+
+	출력
+	첫째 줄에, 42로 나누었을 때, 서로 다른 나머지가 몇 개 있는지 출력한다.
+
+*/
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int result = 0;
+		Queue<Integer> list = new LinkedList<>();
+
+		for (int i = 0; i < 10; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if (!list.contains(num % 42)) {
+				list.offer(num % 42);
+			}
+		}
+
+		System.out.println(list.size());
+
+	}
+
+}
