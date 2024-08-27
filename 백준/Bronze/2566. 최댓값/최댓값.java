7048a914e5ac8a069d7e89e06bf84fd3e2eccb6e
lettuce82 <152861713+lettuce82@users.noreply.github.com>
2024-08-28 08:36:45 +0900

    [Bronze III] Title: 최댓값, Time: 116 ms, Memory: 16044 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -17,29 +17,23 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		int[][] arr = new int[9][9];
+		int max = 0;
+		int row = 0, col = 0;
 
 		for (int i = 0; i < 9; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < 9; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-
-		StringBuilder sb = new StringBuilder();
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				if (arr[i][j] > max) {
-					max = arr[i][j];
-					sb.setLength(0);
-					sb.append(arr[i][j]).append("\n").append((i + 1) + " " + (j + 1));
+				int current = Integer.parseInt(st.nextToken());
+				if (current >= max) {
+					max = current;
+					row = i + 1;
+					col = j + 1;
 				}
 			}
 		}
 
-		System.out.println(sb);
-
+		System.out.println(max);
+		System.out.print(row + " " + col);
 	}
 
 }
