036749aff90b100b43b4f3b6c232010bab6f2a9b
lettuce82 <152861713+lettuce82@users.noreply.github.com>
2024-09-25 09:19:50 +0900

    [Bronze I] Title: 소인수분해, Time: 128 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 // 문제
 // 정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
@@ -12,48 +9,20 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
 
-        if (num > 1) {
-            List<Integer> factors = primeFactorization(num);
-            for (int factor : factors) {
-                System.out.println(factor);
-            }
-        }
-    }
-
-    public static boolean[] sieve(int num) {
-        boolean[] isPrime = new boolean[num + 1];
-        for (int i = 2; i <= num; i++) {
-            isPrime[i] = true;
-        }
-
-        for (int i = 2; i * i <= num; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j <= num; j += i) {
-                    isPrime[j] = false;
-                }
+        for (int i = 2; i < num; i++) {
+            while (num % i == 0) {
+                System.out.println(i);
+                num /= i;
             }
         }
 
-        return isPrime;
-    }
-
-    public static List<Integer> primeFactorization(int num) {
-        List<Integer> factors = new ArrayList<>();
-        boolean[] isPrime = sieve(num);
-
-        for (int i = 2; i <= num; i++) {
-            if (isPrime[i]) {
-                while (num % i == 0) {
-                    factors.add(i);
-                    num /= i;
-                }
-            }
+        if (num != 1) {
+            System.out.println(num);
         }
-
-        return factors;
     }
 
 }
